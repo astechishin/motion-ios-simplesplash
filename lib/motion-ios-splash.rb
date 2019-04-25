5822177181c3798a51e0a50898021e53bd577463
apstechishin <andy@canasoftware.ca>
2019-04-25 15:06:04 +0200

    Initial commit

---
+++ lib/motion-ios-splash.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+unless defined?(Motion::Project::Config)
+  raise "This file must be required within a RubyMotion project Rakefile."
+end
+
+lib_dir_path = File.dirname(File.expand_path(__FILE__))
+
+require File.join(lib_dir_path, "tasks/motion-ios-splash.rb")
